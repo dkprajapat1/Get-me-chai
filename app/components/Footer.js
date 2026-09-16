411b4404433e77ede4d6bd931c3fae6993ab63de
Dinesh Kumar Prajapat <dineshprajapat3137@gmail.com>
2026-09-16 14:54:27 +0530

    update footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="w-full p-3 text-center font-bold underline text-white bg-violet-950 text-sm sm:text-base mt-auto" >
-     &copy; Dinesh Kumar Prajapat 
+    <div className="w-full p-3 text-center font-bold text-white bg-violet-950 text-sm sm:text-base mt-auto" >
+      &copy; 
+     <a target='_blank' href='https://www.linkedin.com/in/dineshkumarprajapat/' className='underline text-blue-500'>Dinesh Kumar Prajapat </a>
+     connect with me on LinkedIn
     </div>
   )
 }
